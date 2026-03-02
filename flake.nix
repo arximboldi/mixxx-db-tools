94706297334f6b094769885b12909aaf03630fc9
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2026-03-02 21:20:16 +0100

    Migrate to flakes and direnv

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Mixxx DB tools";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.sqlitebrowser
+            (pkgs.python3.withPackages (
+              ps: with ps; [
+                tqdm
+                unidecode
+                m3u8
+                ffmpy
+              ]
+            ))
+          ];
+          shellHook = ''
+            export REPO_ROOT=`dirname ${toString ./default.nix}`
+            export PYTHONIOENCODING=utf8
+            addToSearchPath PATH "$REPO_ROOT"
+
+            function make-unique() {
+                name=$1
+                if [[ -e $name || -L $name ]]
+                then
+                    i=0
+                    while [[ -e $name-$i || -L $name-$i ]] ; do
+                        let i++
+                    done
+                    name=$name-$i
+                fi
+                echo $name
+            }
+
+            alias save="cp ~/.mixxx/mixxxdb.sqlite ./mixxxdb.sqlite.backup && \
+                        cp ./mixxxdb.fixed.sqlite ~/.mixxx/mixxxdb.sqlite"
+            alias load="cp ~/.mixxx/mixxxdb.sqlite ./mixxxdb.sqlite"
+            alias restore="cp ./mixxxdb.sqlite.backup ~/.mixxx/mixxxdb.sqlite"
+            alias next="cp ./mixxxdb.fixed.sqlite ./mixxxdb.sqlite"
+
+            function backup() {
+                cp ./mixxxdb.sqlite "$(make-unique "./mixxxdb.sqlite.backup.$(date '+%Y-%m-%d')")"
+            }
+          '';
+        };
+      }
+    );
+}
